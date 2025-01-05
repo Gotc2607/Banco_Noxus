918a40d4a6ce2e3dd931ff5f208e4c081ad672b3
Gotc2607 <giovanicoelho2005@gmail.com>
2025-01-05 14:32:45 -0300

    Update interface_banco.py
    Arrumando o código

--- interface_banco.py
+++ interface_banco.py
@@ -12,11 +12,11 @@ class menu_interface(banco_de_dados2):
         self.janela.title("Menu Principal")
         self.janela.geometry('400x300')
         self.janela.resizable(False, False)
-        self.tela_login()  # Exibe a tela de login
+        self.tela_login() 
 
-    #tela principal para os usuarios já cadastrados
+    
     def tela_principal(self, usuario, senha, saldo):
-        # Tela principal com opções (Depósito, Saque, etc.)
+        
         self.limpar_tela()
 
         self.usuario_atual = usuario
@@ -29,14 +29,14 @@ class menu_interface(banco_de_dados2):
         else:
             tk.Label(self.janela, text='Erro ao carregar o saldo.', font=('Arial', 14),fg='red', bg='black').pack(pady=10)
 
-        tk.Button(self.janela, text='Depositar', command=self.tela_deposito, fg='red', bg='gray').pack(pady=5) #botão de deposito
+        tk.Button(self.janela, text='Depositar', command=self.tela_deposito, fg='red', bg='gray').pack(pady=5) 
 
-        tk.Button(self.janela, text='Sacar', command=self.tela_saque, fg='red', bg='gray').pack(pady=5)# botão de saque
+        tk.Button(self.janela, text='Sacar', command=self.tela_saque, fg='red', bg='gray').pack(pady=5)
 
         tk.Button(self.janela, text='Sair', command=self.finalizar_sessão, fg='red', bg='gray').pack(pady=5)
 
     def tela_login(self):
-        # Tela de login
+        
         self.limpar_tela()
 
         self.janela.configure(bg='black')
@@ -47,42 +47,42 @@ class menu_interface(banco_de_dados2):
 
         tk.Label(self.janela, text= 'Junte-se a Noxus', font=('Arial', 16), fg='red', bg='black').grid(row=0, column=0, columnspan=2, pady=5)
 
-        #parte do usuario
+        
         tk.Label(self.janela, text='Usuario:', fg='red', bg='black').grid(row=1, column=1, columnspan=2, padx=5, pady=5, sticky='w')
         self.nome_usuario = tk.Entry(self.janela, bg='grey')
         self.nome_usuario.grid(row=1, column=1, columnspan=2, padx=5, pady=5)
 
-        #parte da senha
+        
         tk.Label(self.janela, text='Senha:', fg='red', bg='black').grid(row=2, column=1, columnspan=2, padx=5, pady=5, sticky='w')
         self.senha_usuario = tk.Entry(self.janela, show='*', bg='grey')
         self.senha_usuario.grid(row=2, column=1, columnspan=2, padx=5, pady=5)
 
-        tk.Button(self.janela, text='Login', command=self.fazer_login, fg='red', bg='grey').grid(row=5, column=1,pady=3) #botão para fazer login
-        tk.Button(self.janela, text='Cadastrar', command= self.tela_novo_usuario,fg='red', bg='grey').grid(row=5, column=0,pady=3) #botão parase cadastrar
+        tk.Button(self.janela, text='Login', command=self.fazer_login, fg='red', bg='grey').grid(row=5, column=1,pady=3) 
+        tk.Button(self.janela, text='Cadastrar', command= self.tela_novo_usuario,fg='red', bg='grey').grid(row=5, column=0,pady=3) 
         tk.Button(self.janela, text='Sair', command=self.finalizar, fg='red', bg='grey').grid(row=6, column=0, columnspan=2, pady=5)
 
-    def fazer_login(self): # metodo intermediario para receber os valores de usuario e senha
-    # Obtém os valores dos campos de entrada
+    def fazer_login(self): 
+    
         usuario = self.nome_usuario.get()
         senha = self.senha_usuario.get()
 
-    # Chama o método verificar_usuario e trata o resultado
+    
         if self.verificar_usuario(usuario, senha):
             messagebox.showinfo("Login bem-sucedido", f"Bem-vindo, {usuario}!")
             saldo = self.ver_saldo(senha)
-            self.tela_principal(usuario, senha, saldo)  # Navega para a tela do usuário
+            self.tela_principal(usuario, senha, saldo)  
         else:
             messagebox.showerror("Erro de Login", "Usuário ou senha inválidos!")
 
     def tela_novo_usuario(self):
-        # Tela para cadastrar novo usuário
+        
         self.limpar_tela()
 
         tk.Label(self.janela, text='Cadastre-se aqui', font=('Arial', 16), fg='red', bg='black').pack(pady=10)
 
         tk.Label(self.janela, text='Nome de usuario:', fg='red', bg='black').pack(pady=10)
         self.nome_usuario = tk.Entry(self.janela,bg='gray')
-        self.nome_usuario.pack(pady=10) #area para colocar o nome do novo usuario
+        self.nome_usuario.pack(pady=10) 
 
         tk.Label(self.janela, text='Crie uma senha:', fg='red', bg='black').pack(pady=10)
         self.senha_usuario = tk.Entry(self.janela, show='*', bg='gray')
@@ -92,7 +92,7 @@ class menu_interface(banco_de_dados2):
 
 
     def cadastrar_usuario(self):
-        # Função para cadastrar um novo usuário
+    
         Nusuario = self.nome_usuario.get()
         Nsenha = self.senha_usuario.get()
 
@@ -117,16 +117,16 @@ class menu_interface(banco_de_dados2):
                 sucesso = self.depositar(self.senha_atual, valor)
                 if sucesso:
                     messagebox.showinfo("Sucesso", f"Depósito de R${valor:.2f} realizado com sucesso!")
-                    self.tela_principal(self.usuario_atual, self.senha_atual, self.ver_saldo(self.senha_atual))  # Volta para a tela principal
+                    self.tela_principal(self.usuario_atual, self.senha_atual, self.ver_saldo(self.senha_atual))  
                 else:
                     messagebox.showerror("Erro", "Falha ao realizar o depósito. Verifique os dados.")
             except ValueError:
                 messagebox.showerror("Erro", "Por favor, insira um valor válido.")
 
-        # Botão para confirmar o depósito
+        
         tk.Button(self.janela, text='Confirmar Depósito', command=realizar_deposito, fg='red', bg='gray').pack(pady=10)
 
-        # Botão para voltar para a tela principal
+        
         tk.Button(self.janela, text='Voltar', command=lambda: self.tela_principal(self.usuario_atual, self.senha_atual, self.ver_saldo(self.senha_atual)), fg='red', bg='gray').pack(pady=10)
     
     
@@ -145,16 +145,16 @@ class menu_interface(banco_de_dados2):
                 sucesso = self.sacar(self.senha_atual, valor)
                 if sucesso:
                     messagebox.showinfo("Sucesso", f"Saque de R${valor:.2f} realizado com sucesso!")
-                    self.tela_principal(self.usuario_atual, self.senha_atual, self.ver_saldo(self.senha_atual))  # Volta para a tela principal
+                    self.tela_principal(self.usuario_atual, self.senha_atual, self.ver_saldo(self.senha_atual)) 
                 else:
                     messagebox.showerror("Erro", "Falha ao realizar o saque. Verifique os dados.")
             except ValueError:
                 messagebox.showerror("Erro", "Por favor, insira um valor válido.")
 
-        # Botão para confirmar o depósito
+        
         tk.Button(self.janela, text='Confirmar Saque', command=realizar_saque, fg='red', bg='gray').pack(pady=10)
 
-        # Botão para voltar para a tela principal
+        
         tk.Button(self.janela, text='Voltar', command=lambda:self.tela_principal(self.usuario_atual, self.senha_atual, self.ver_saldo(self.senha_atual)), fg='red', bg='gray').pack(pady=10)
 
     def finalizar_sessão(self):
@@ -168,7 +168,6 @@ class menu_interface(banco_de_dados2):
         self.janela.destroy()
 
     def limpar_tela(self):
-        # Remove todos os widgets da janela atual
         for widget in self.janela.winfo_children():
             widget.destroy()
     
