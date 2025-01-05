4f9704604d7923963a11262a835992560d12058c
Gotc2607 <giovanicoelho2005@gmail.com>
2025-01-05 14:36:10 -0300

    Update banco_de_dados2.py
    arrumando o banco de dados

--- banco_de_dados2.py
+++ banco_de_dados2.py
@@ -15,7 +15,7 @@ class banco_de_dados2:
             saldo REAl DEFAULT 0
         );
         ''')
-        #verificar se já existe esse nome de usuario
+        
     def verificar_nome_usuari(self, usuario):
         self.cursor.execute("SELECT * FROM banco_de_dados WHERE usuario = ?", (usuario,))
         resultado = self.cursor.fetchone()
@@ -24,7 +24,7 @@ class banco_de_dados2:
         else:
             return False
        
-        #verificar se a senha e o nome de usuario então certos
+        
     def verificar_usuario(self, usuario, senha):
         self.cursor.execute("SELECT * FROM banco_de_dados WHERE usuario = ? AND senha = ?", (usuario, senha))
         resultado = self.cursor.fetchone()
@@ -39,56 +39,56 @@ class banco_de_dados2:
 
     def depositar(self, senha, valor):
         try:
-            # Consulta o saldo pelo campo senha
+            
             self.cursor.execute('SELECT saldo FROM banco_de_dados WHERE senha = ?', (senha,))
             saldo_atual = self.cursor.fetchone()
             saldo_atual = saldo_atual[0]
 
-        # Verifica se a senha é válida
+        
             if saldo_atual is None:
                 return False
 
-        # Valida o valor do depósito
+        
             if not isinstance(valor, (int, float)) or valor <= 0:
                 return False
 
-        # Calcula o novo saldo e atualiza no banco
+        
             saldo_novo = saldo_atual + valor
             self.cursor.execute('UPDATE banco_de_dados SET saldo = ? WHERE senha = ?', (saldo_novo, senha))
 
-        # Exibe o saldo atualizado e retorna sucesso
+        
             return True
 
         except sqlite3.Error as e:
-        # Trata erros de banco de dados
+        
             return False
             
     def sacar(self, senha, valor):
         try:
-            # Consulta o saldo pelo campo senha
+        
             self.cursor.execute('SELECT saldo FROM banco_de_dados WHERE senha = ?', (senha,))
             saldo_atual = self.cursor.fetchone()
 
-        # Verifica se a senha é válida
+        
             if saldo_atual is None:
                 return False
 
-        # Extrai o saldo atual
-            saldo_atual = saldo_atual[0]  # Extrai o valor numérico da tupla
+        
+            saldo_atual = saldo_atual[0]  
 
-        # Valida o valor do saque
+        
             if not isinstance(valor, (int, float)) or valor > saldo_atual:
                 return False
 
-        # Calcula o novo saldo e atualiza no banco
+        
             saldo_novo = saldo_atual - valor
             self.cursor.execute('UPDATE banco_de_dados SET saldo = ? WHERE senha = ?', (saldo_novo, senha))
 
-        # Exibe o saldo atualizado e retorna sucesso
+        
             return True
 
         except sqlite3.Error as e:
-        # Trata erros de banco de dados
+        
             return False
 
     def ver_saldo(self, senha):
